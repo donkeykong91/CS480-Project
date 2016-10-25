dcac592ad60a59dc32c956611c20040d6470ee89
Miriam Gutierrez <mirigu93@gmail.com>
2016-10-24 18:07:18 -0700

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ManageDevices
+{
+    public partial class Form2 : Form
+    {
+        Main form1;
+        public Form2(Main form_1)
+        {
+            InitializeComponent();
+            form1 = form_1;
+        }
+
+        // For the 'No' button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //For the 'Yes' button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = (DirectoryInfo)form1.treeView1.SelectedNode.Tag;
+            File.WriteAllText(Path.Combine(dir.FullName, "test1234.txt"), "Testing");
+            this.Close();
+        }
+    }
+}
