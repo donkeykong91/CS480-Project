1e8d54bc5ad978ccf126e6e7dda1c0bf4d2f8cc8
Joshua Whitney <joshwhtny@yahoo.com>
2016-11-16 10:35:42 -0800

    Methods for file syncing in progress
    This is a collection of methods to add functionality to the GUI. Contains methods for searching, importing, exporting, comparing files. To be added to final implementation of GUI. Currently a work in progress -NOT FINISHED-

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandlerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandPrompt instance = new CommandPrompt();
+            instance.Run();
+        }
+    }
+}
