04f638ed066c4a90f86646e4a0dfc85ca39e445a
gabrieltal <gabrielvt6@gmail.com>
2016-11-22 21:13:19 -0800

    Update Form1.Designer.cs
    Changed start position to be centered in the middle of the screen

--- ManageDevices/Form1.Designer.cs
+++ ManageDevices/Form1.Designer.cs
@@ -129,6 +129,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main Menu";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
